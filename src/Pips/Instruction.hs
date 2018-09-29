6f5fb20a0442a0bbb1c14c7bde5ee66c020d3ad5
David Veenstra <davidjulianveenstra@gmail.com>
2018-09-29 13:54:46 +0200

    Replace instance of Enum for OpCode en AluOp with function
    Since the op code and funct/alu op are not sequential numbers,
    it doesn't necessarily make sense to use an enum instance. So we use
    regular functions instead.

--- src/Pips/Instruction.hs
+++ src/Pips/Instruction.hs
@@ -6,24 +6,25 @@ import Pips.Common
 
 data OpCode = ROpc | AddiOpc | LuiOpc | LwOpc | SwOpc | BeqOpc | BneOpc | JOpc deriving (Eq, Show)
 
-instance Enum OpCode where
-  toEnum 0x00 = ROpc
-  toEnum 0x0c = AddiOpc
-  toEnum 0x0f = LuiOpc
-  toEnum 0x23 = LwOpc
-  toEnum 0x2b = SwOpc
-  toEnum 0x04 = BeqOpc
-  toEnum 0x05 = BneOpc
-  toEnum 0x02 = JOpc
+opcodeToInt :: OpCode -> Int
+opcodeToInt AddiOpc= 0x0c
+opcodeToInt LuiOpc = 0x0f
+opcodeToInt LwOpc  = 0x23
+opcodeToInt SwOpc  = 0x2b
+opcodeToInt BeqOpc = 0x04
+opcodeToInt BneOpc = 0x05
+opcodeToInt JOpc   = 0x02
+opcodeToInt ROpc   = 0x00
 
-  fromEnum ROpc   = 0x00
-  fromEnum AddiOpc= 0x0c
-  fromEnum LuiOpc = 0x0f
-  fromEnum LwOpc  = 0x23
-  fromEnum SwOpc  = 0x2b
-  fromEnum BeqOpc = 0x04
-  fromEnum BneOpc = 0x05
-  fromEnum JOpc   = 0x02
+intToOpCode :: Int -> OpCode
+intToOpCode 0x0c = AddiOpc
+intToOpCode 0x0f = LuiOpc
+intToOpCode 0x23 = LwOpc
+intToOpCode 0x2b = SwOpc
+intToOpCode 0x04 = BeqOpc
+intToOpCode 0x05 = BneOpc
+intToOpCode 0x02 = JOpc
+intToOpCode _    = ROpc
 
 data Instruction =
   Instruction {
@@ -44,35 +45,31 @@ data InstructionType = R | I | J deriving (Show, Eq)
 
 data AluOp = AddOp | SubOp | MulOp | AndOp | OrOp | SllOp | SrlOp | SltOp | JrOp | XorOp | LuiOp deriving (Show, Eq)
 
-instance Enum AluOp where
-  toEnum 0x18 = MulOp
-  toEnum 0x20 = AddOp
-  toEnum 0x22 = SubOp
-  toEnum 0x24 = AndOp
-  toEnum 0x25 = OrOp
-  toEnum 0x00 = SllOp
-  toEnum 0x02 = SrlOp
-  toEnum 0x2a = SltOp
-  toEnum 0x08 = JrOp
-  toEnum 0x26 = XorOp
+aluOpToInt :: AluOp -> Int
+aluOpToInt MulOp = 0x18
+aluOpToInt AddOp = 0x20
+aluOpToInt SubOp = 0x22
+aluOpToInt AndOp = 0x24
+aluOpToInt OrOp  = 0x25
+aluOpToInt SllOp = 0x00
+aluOpToInt SrlOp = 0x02
+aluOpToInt SltOp = 0x2a
+aluOpToInt JrOp  = 0x08
+aluOpToInt XorOp = 0x26
+aluOpToInt LuiOp = 0x50 -- non conform mips
 
-  -- non conform mips
-  toEnum 0x50 = LuiOp
-  toEnum _    = SllOp
-
-  fromEnum MulOp = 0x18
-  fromEnum AddOp = 0x20
-  fromEnum SubOp = 0x22
-  fromEnum AndOp = 0x24
-  fromEnum OrOp  = 0x25
-  fromEnum SllOp = 0x00
-  fromEnum SrlOp = 0x02
-  fromEnum SltOp = 0x2a
-  fromEnum JrOp  = 0x08
-  fromEnum XorOp = 0x26
-
-  -- non conform mips
-  fromEnum LuiOp = 0x50
+intToAluOp :: Int -> AluOp
+intToAluOp 0x18 = MulOp
+intToAluOp 0x20 = AddOp
+intToAluOp 0x22 = SubOp
+intToAluOp 0x24 = AndOp
+intToAluOp 0x25 = OrOp
+intToAluOp 0x2a = SltOp
+intToAluOp 0x08 = JrOp
+intToAluOp 0x26 = XorOp
+intToAluOp 0x50 = LuiOp -- non-conform mips
+intToAluOp 0x02 = SrlOp
+intToAluOp _    = SllOp
 
 nop :: Instruction
 nop = Instruction ROpc R 0 0 0 0 0 AddOp "" 0 0
